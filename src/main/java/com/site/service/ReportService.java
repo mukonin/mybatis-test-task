460b372aff5ebd0ef30ab3e5f0e92066c9fc54f7
mukonin <alex.mukonin@gmail.com>
2017-10-25 20:33:40 +0300

    EOD commit, ReportService, wip

--- src/main/java/com/site/service/ReportService.java
+++ src/main/java/com/site/service/ReportService.java
@@ -1,9 +1,12 @@
 package com.site.service;
 
 import com.site.model.DocumentFormat;
+import com.site.model.MailDTO;
 import com.site.model.Report;
 import com.site.repository.ReportRepository;
+import com.site.util.ExcelUtil;
 import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,8 +16,36 @@ import java.util.List;
 public class ReportService {
 
 	private final ReportRepository repository;
+	private final Environment environment;
 
 	public void sendReport(String email, DocumentFormat format, String filter) {
 		List<Report> reports = repository.findAll();
+		Object[][] tableData = reports
+				.stream()
+				.map(ReportService::mapToFieldsArray).toArray(Object[][]::new);
+
+		byte[] attachment = ExcelUtil.convert(tableData);
+
+		String[] to = new String[1];
+		to[0] = email;
+		MailDTO mail = new MailDTO();
+		mail.setFrom(environment.getProperty("email.from"));
+		mail.setTo(to);
+		mail.setSubject("Report");
+		mail.setBody("Report");
+		mail.setAttachmentName("report.xls");
+		mail.setAttachment(attachment);
+	}
+
+	private static Object[] mapToFieldsArray(Report report) {
+		Object[] row = new Object[7];
+		row[0] = report.getCountryName();
+		row[1] = report.getUserName();
+		row[2] = report.getUrl();
+		row[3] = report.getMethod();
+		row[4] = report.getParams();
+		row[5] = report.getRequestId();
+		row[6] = report.getSessionId();
+		return row;
 	}
 }
