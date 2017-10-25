e9e900a0bc94d39e296931e594334b5e997a0427
mukonin <alex.mukonin@gmail.com>
2017-10-25 11:31:26 +0300

    Refactor

--- src/main/java/com/site/repository/domain/UserGroupRepository.java
+++ src/main/java/com/site/repository/domain/UserGroupRepository.java
@@ -6,7 +6,7 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserGroupRepository extends BaseRepository<UserGroup, UserGroupMapper> implements  CrudRepository<UserGroup> {
+public class UserGroupRepository extends BaseRepository<UserGroup, UserGroupMapper> implements CrudRepository<UserGroup> {
 
 	public UserGroupRepository(SqlSessionFactory sessionFactory) {
 		super(sessionFactory, UserGroupMapper.class);
