b1f24da32de64c6ab23ec30d941bae2ef2444bc8
mukonin <alex.mukonin@gmail.com>
2017-10-26 00:41:29 +0300

    report service

--- src/main/java/com/site/service/ReportService.java
+++ src/main/java/com/site/service/ReportService.java
@@ -5,18 +5,32 @@ import com.site.model.MailDTO;
 import com.site.model.Report;
 import com.site.repository.ReportRepository;
 import com.site.util.ExcelUtil;
+import com.site.util.WordUtil;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class ReportService {
 
 	private final ReportRepository repository;
+	private final MailService mailService;
 	private final Environment environment;
+
+	private static final String REPORT_SUBJECT_BODY = "Report";
+	private static final Map<DocumentFormat, String> FILE_NAMES = new EnumMap<>(DocumentFormat.class);
+
+	static {
+		FILE_NAMES.put(DocumentFormat.XLS, "report.xls");
+		FILE_NAMES.put(DocumentFormat.DOC, "report.doc");
+	}
 
 	public void sendReport(String email, DocumentFormat format, String filter) {
 		List<Report> reports = repository.findAll();
@@ -24,17 +38,25 @@ public class ReportService {
 				.stream()
 				.map(ReportService::mapToFieldsArray).toArray(Object[][]::new);
 
-		byte[] attachment = ExcelUtil.convert(tableData);
+		byte[] attachment = getAttachment(tableData, format);
 
-		String[] to = new String[1];
-		to[0] = email;
-		MailDTO mail = new MailDTO();
-		mail.setFrom(environment.getProperty("email.from"));
-		mail.setTo(to);
-		mail.setSubject("Report");
-		mail.setBody("Report");
-		mail.setAttachmentName("report.xls");
-		mail.setAttachment(attachment);
+		MailDTO mail = buildMail(email, attachment, format);
+		mailService.sendMail(mail);
+	}
+
+	private static byte[] getAttachment(Object[][] tableData, DocumentFormat format) {
+		byte[] result;
+		switch (format) {
+			case DOC:
+				result = WordUtil.convert(tableData);
+				break;
+			case XLS:
+				result = ExcelUtil.convert(tableData);
+				break;
+			default:
+				throw new IllegalArgumentException("Unknown data format " + format.name());
+		}
+		return result;
 	}
 
 	private static Object[] mapToFieldsArray(Report report) {
@@ -47,5 +69,18 @@ public class ReportService {
 		row[5] = report.getRequestId();
 		row[6] = report.getSessionId();
 		return row;
+	}
+
+	private MailDTO buildMail(String email, byte[] attachment, DocumentFormat format) {
+		String[] to = new String[1];
+		to[0] = email;
+		MailDTO mail = new MailDTO();
+		mail.setFrom(environment.getProperty("mail.from"));
+		mail.setTo(to);
+		mail.setSubject(REPORT_SUBJECT_BODY);
+		mail.setBody(REPORT_SUBJECT_BODY);
+		mail.setAttachmentName(FILE_NAMES.get(format));
+		mail.setAttachment(attachment);
+		return mail;
 	}
 }
